c1ef5563f5a2683b87c64b84e060c9c7a766cbb1
kenne roberto <krsilva4@hotmail.com>
2019-07-05 11:52:45 -0300

    PUT, DELETE e GET para clientes

---
+++ src/main/java/com/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,57 @@
+package com.cursomc.dto;
+
+import java.io.Serializable;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+
+	//Realizando consistencia no campo abaixo
+	@NotEmpty(message = "Preenchimento obrigatorio!")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 a 80 caracteres")
+	private String nome;
+	@NotEmpty(message = "Preenchimento obrigatorio!")
+	@Email(message = "Email invalido!")
+	private String email;
+
+	public ClienteDTO() {
+	}
+
+	public ClienteDTO(Cliente obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.email = obj.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
--- src/main/java/com/cursomc/services/ClienteService.java
+++ src/main/java/com/cursomc/services/ClienteService.java
@@ -1,10 +1,19 @@
 package com.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cursomc.domain.Cliente;
+import com.cursomc.domain.Cliente;
+import com.cursomc.dto.ClienteDTO;
 import com.cursomc.repositories.ClienteRepository;
+import com.cursomc.services.exceptions.DataIntegrityException;
 import com.cursomc.services.exceptions.ObjectNotFoundException;
 
 /*Camada de servicos.
@@ -14,15 +23,74 @@ import com.cursomc.services.exceptions.ObjectNotFoundException;
 @Service
 public class ClienteService {
 
+	private final String msgErro = "Nao e possivel excluir, porque ha entidades relacionadas!";
 	@Autowired
 	private ClienteRepository repo;
 
 	public Cliente find(Integer id) {
 		Cliente obj = repo.findOne(id);
 // Tratamento de exceção personalizada...		
-		if(obj == null) {
-			throw new ObjectNotFoundException("Objeto nao encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName());
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto nao encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName());
 		}
 		return obj;
+	}
+
+	/*
+	 * Medoto responsavel por criar uma nova cliente
+	 */
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	/*
+	 * Medoto responsavel por atualizar a cliente
+	 */
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(obj , newObj); 
+		return repo.save(newObj);
+	}
+
+	/*
+	 * Medoto responsavel por deletar a cliente
+	 */
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException(msgErro);
+		}
+
+	}
+
+	public List<Cliente> fidAll() {
+		// TODO Auto-generated method stub
+		return repo.findAll();
+	}
+
+	/*
+	 * Medoto responsavel por paginacao e ordenacao dos registros.
+	 */
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	/*
+	 * Medoto responsavel para converter uma DTO para cliente.
+	 */
+	public Cliente fromDTO(ClienteDTO obj) {
+		return new Cliente(obj.getId(), obj.getNome(), obj.getEmail(), null , null);
+	}
+	/*
+	 * Medoto responsavel para conpletar dados vindo da API com dados vindo da tabela.
+	 */
+	private void updateData(Cliente obj , Cliente newObj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
