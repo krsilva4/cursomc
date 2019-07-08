da29861d496aea7ebe27dd8685a70ca2d116eff3
kenne roberto <krsilva4@hotmail.com>
2019-07-08 11:51:39 -0300

    43. Validação customizada email não repetido na insercao

--- src/main/java/com/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/cursomc/repositories/ClienteRepository.java
@@ -2,14 +2,17 @@ package com.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.cursomc.domain.Cliente;;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.cursomc.domain.Cliente;;
 
 /*Camada de acesso a dados obs(Conversar com o banco de dados , contendo todas as operacoes do 
 	banco de dados exemplo consultas, inserts, updates, deletes)
 */
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
-
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+    //Metodo responsavel de verificar se existe cliente para tal email.
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cursomc.domain.Cliente;
 import com.cursomc.domain.enums.TipoCliente;
 import com.cursomc.dto.ClienteNewDTO;
+import com.cursomc.repositories.ClienteRepository;
 import com.cursomc.resources.exception.FieldMessage;
 import com.cursomc.services.validation.utils.BR;
-  
-public class ClienteInsertValidator implements  
-ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	public ClienteRepository repo;
+
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -21,13 +28,19 @@ ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
 		// validacao de cpf
-		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod())&& !BR.isCpfValido(objDto.getCpfOuCnpj())) {
-		list.add(new FieldMessage("cpfOuCnpj", "Cpf invalido"));
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isCpfValido(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "Cpf invalido"));
 		}
 		// validacao de cnpj
-		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod())&& !BR.isCnpjValido(objDto.getCpfOuCnpj())) {
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isCnpjValido(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "Cnpj invalido"));
-			}
+		}
+		// validacao de cliente
+		Cliente cli = repo.findByEmail(objDto.getEmail());
+		if (cli != null) {
+			list.add(new FieldMessage("email", "Email ja existente"));
+		}
+
 // inclua os testes aqui, inserindo erros na lista
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
